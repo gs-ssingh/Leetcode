8fcdd8bd2c61482e1432d4058d7fd9d4e47b751a
Sumeet <ssingh6466@gmail.com>
2018-10-02 23:56:41 -0400

    Commit : 2nd Oct. 3 sum

--- src/algorithms/Three_Sum_015.java
+++ src/algorithms/Three_Sum_015.java
@@ -1,38 +1,38 @@
 package algorithms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 public class Three_Sum_015 {
     public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        int i = 0, j = nums.length - 1;
-        boolean flag = true;
-        while (i < j) {
-            int a = i, b = j;
-            while (a < b) {
-                int k = (a + b) / 2;
+        if (nums.length < 3) {
+            return res;
+        }
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1, k = nums.length - 1;
+            while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
                 if (sum == 0) {
                     ArrayList<Integer> list = new ArrayList<>();
                     list.add(nums[i]);
-                    list.add(nums[k]);
                     list.add(nums[j]);
+                    list.add(nums[k]);
                     res.add(list);
-                    break;
-                } else if (sum > 0) {
-                    b = k - 1;
+                    while (j < k && nums[j] == nums[++j]) ;
+                    while (j < k && nums[k] == nums[--k]) ;
+                } else if (sum < 0) {
+                    j++;
                 } else {
-                    a = k + 1;
+                    k--;
                 }
-            }
-            if (flag) {
-                i++;
-                flag = !flag;
-            } else {
-                j--;
-                flag = !flag;
             }
         }
         return res;
